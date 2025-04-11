d93648aae3e090cc058b8930c5f4900ee81aad99
felpsss-leudb <feesouza101@gmail.com>
2025-04-11 08:23:07 -0300

    testes

--- app/controllers/usuarioController.js
+++ app/controllers/usuarioController.js
@@ -3,7 +3,7 @@ const {body, validationResult} = require("express-validator");
 
 const usuarioController = {
 
-    //validação
+    //validação formulário
         validacaoFormCad : [
             body("nome").isLength({min:3,max:45})
                 .withMessage("Nome de usuario deve ter no mínimo 3 letras e no máximo 45 letras"),
@@ -21,6 +21,20 @@ const usuarioController = {
             })
         ],
 
+    //validação cliente
+        validacaoCliente: [
+            body("nome")
+              .isLength({ min: 3, max: 45 })
+              .withMessage("Nome do cliente deve conter de 5 a 45 letras!"),
+            body("nomeUsuario")
+              .isLength({ min: 3, max: 45 })
+              .withMessage("Nome de usuário do cliente deve conter de 5 a 45 caracteres!"),
+            body("email")
+                .isEmail(),
+            body("senha")
+                .isStrongPassword(),
+          ],
+
     //métodos 
 
     cadastrar: async (req, res)=>{
@@ -34,6 +48,46 @@ const usuarioController = {
                     "nome": req.body.nome,
                     "email":req.body.email,
                     "senha":req.body.senha
+                };
+                let resultado = await usuarioModel.create(dadosFormulario);
+
+                if(resultado){
+                    return res.render("pages/index");
+                }else{
+                    return res.render("pages/cadastro", 
+                        {"listaErros":null, "valores":req.body}
+                    )
+                }
+
+            }else{
+                //erro no formulário
+                res.render("pages/cadastro", 
+                    {"listaErros":listaErros, "valores":req.body});
+            }
+
+
+        }catch(error){
+            console.log(error);
+            return false;
+        }
+    },
+
+    adicionarCliente: async (req, res)=>{
+
+        try{
+
+            let listaErros = validationResult(req);
+            if( listaErros.isEmpty() ){
+                //não tem erro no formulário
+                const dadosFormulario = {
+                    "nome": req.body.nome,
+                    "cep":req.body.cep,
+                    "ImagemPerfil":req.body.ImagemPerfil,
+                    "nomeUsuario":req.body.nomeUsuario,
+                    "email":req.body.email,
+                    "senha":req.body.senha,
+                    "tipo":req.body.tipo,
+                    "status":req.body.status
                 };
                 let resultado = await usuarioModel.create(dadosFormulario);
 
